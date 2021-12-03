269224a8f2dec70161141da1712355e39bca8bd6
JIWON <87870107+jeewon-bang@users.noreply.github.com>
2021-12-03 10:48:08 +0900

    [Fix] directory update

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import PostList from 'components/community/PostList';
+import PostList from 'components/community/postlist/PostList';
 import MyPageNav from 'components/MyPageForm/Nav/MyPageNav';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
