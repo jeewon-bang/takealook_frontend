a27e007eef4a72a876edeef44d6383142f0b1a6e
hyemin <hmjang28@gmail.com>
2021-12-06 19:23:41 +0900

    [Add] Modal 컴포넌트화,  CareCalendar 돌봄아이콘 띄우기

--- src/components/CatDetail/CatCare/CatCare.js
+++ src/components/CatDetail/CatCare/CatCare.js
@@ -1,31 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import Modal from 'components/common/Modal';
 import './CatCare.scss';
+import CareCalendar from '../CareCalendar/CareCalendar';
 
 const CatCare = (props) => {
-	const { catId, showModal, setShowModal, care, setCare } = props;
+	const { catId, careHistory, setCareHistory } = props;
+	const [showModal, setShowModal] = useState(false);
 	const today = moment();
+
 	const timeDiff = (date) => {
 		return Math.floor(
 			moment.duration(today.diff(moment(date, 'yyyy-MM-DD HH:mm'))).asHours()
 		);
 	};
 
+	const openModal = () => {
+		setShowModal(true);
+	};
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
+
 	return (
-		<div className='care-box'>
-			<div className='care-header'>
+		<div className='care-container'>
+			<div className='cat-info-title-text'>
 				최근 48시간의 돌봄 기록
-				<span onClick={() => setShowModal(true)}> [캘린더보기]</span>
+				<span onClick={openModal}> [캘린더보기]</span>
 			</div>
-			{care.map((v) => (
-				<div className='care'>
-					<div className='user-img'></div>
-					<span>{v.user} / </span>
-					<span>{timeDiff(v.time)}시간 전/</span>
-					<span>{v.type} / </span>
-					<span>{v.message}</span>
-				</div>
-			))}
+			<div className='care-box'>
+				{careHistory.map((v) => (
+					<div className='care'>
+						<div className='user-img'></div>
+						<span>{v.user} / </span>
+						<span>{timeDiff(v.datetime)}시간 전/</span>
+						<span>{v.type} / </span>
+						<span>{v.message}</span>
+					</div>
+				))}
+			</div>
+			{/* 모달 사용하기 
+				- showModal 변수 정의, openModal, closeModal 함수 정의
+				- { showModal && <Modal showModal={showModal} maskClosable={true} onClose={closeModal}>안에넣을내용</Modal> } 
+				  형태로 사용하기
+			 */}
+			{showModal && (
+				<Modal showModal={showModal} maskClosable={true} onClose={closeModal}>
+					<CareCalendar
+						careHistory={careHistory}
+						setCareHistory={setCareHistory}
+					/>
+				</Modal>
+			)}
 		</div>
 	);
 };
--- src/pages/CatDetail/CatDetailPage.js
+++ src/pages/CatDetail/CatDetailPage.js
@@ -1,63 +1,119 @@
 import React, { useState } from 'react';
-import ReactDatePicker from 'react-datepicker';
 import { useParams } from 'react-router';
-import CareCalendar from '../../components/CatDetail/CareCalendar/CareCalendar';
-import CatCare from '../../components/CatDetail/CatCare/CatCare';
-import CatInfo from '../../components/CatDetail/CatInfo/CatInfo';
+import CareCalendar from 'components/CatDetail/CareCalendar/CareCalendar';
+import CatCare from 'components/CatDetail/CatCare/CatCare';
+import CatInfo from 'components/CatDetail/CatInfo/CatInfo';
+import CatMap from 'components/CatDetail/CatMap/CatMap';
 import './CatDetailPage.scss';
+import { Link } from 'react-router-dom';
 
+// 0 : 밥, 1: 간식, 2: 약, 3:병원  4:기타
 let data = [
-  {
-    id: 1,
-    user: '혜민',
-    userImg: '',
-    time: '2021-11-10 12:40:33',
-    type: '사료 급여',
-    message: '사료 바꿨어요!',
-  },
-  {
-    id: 2,
-    user: '지원',
-    userImg: '',
-    time: '2021-11-29 22:24:10',
-    type: '약 급여',
-    message: '',
-  },
-  {
-    id: 3,
-    user: '세은',
-    userImg: '',
-    time: '2021-11-30 10:55:00',
-    type: '치료',
-    message: '구내염 처방받으러 병원갔다왔어요ㅠㅠ',
-  },
+	{
+		id: 1,
+		user: '혜민',
+		userImg: '',
+		datetime: '2021-11-10 12:40:33',
+		type: 0,
+		message: '사료 바꿨어요!',
+	},
+	{
+		id: 2,
+		user: '지원',
+		userImg: '',
+		datetime: '2021-11-29 22:24:10',
+		type: 2,
+		message: '',
+	},
+	{
+		id: 3,
+		user: '세은',
+		userImg: '',
+		datetime: '2021-11-30 10:55:00',
+		type: 3,
+		message: '구내염 처방받으러 병원갔다왔어요ㅠㅠ',
+	},
+	{
+		id: 4,
+		user: '지수',
+		userImg: '',
+		datetime: '2021-11-30 14:55:00',
+		type: 2,
+		message: '츄르 짭짭',
+	},
+	{
+		id: 5,
+		user: '지혜',
+		userImg: '',
+		datetime: '2021-11-30 15:55:00',
+		type: 2,
+		message: '앗 나도 간식줬는데',
+	},
+
+	{
+		id: 6,
+		user: '혜민',
+		userImg: '',
+		datetime: '2021-11-30 15:55:00',
+		type: 0,
+		message: '밥먹자',
+	},
+	{
+		id: 7,
+		user: '혜민',
+		userImg: '',
+		datetime: '2021-11-30 15:55:00',
+		type: 1,
+		message: '간식먹쟝',
+	},
+	{
+		id: 7,
+		user: '지혜',
+		userImg: '',
+		datetime: '2021-11-30 20:55:00',
+		type: 2,
+		message: '귀여우니까 간식 또줘야징',
+	},
 ];
 
 const CatDetailPage = () => {
-  const { catId } = useParams();
-  const [showModal, setShowModal] = useState(false);
-  const [care, setCare] = useState(data);
+	const { catId } = useParams();
+	const [showModal, setShowModal] = useState(false);
+	const [careHistory, setCareHistory] = useState(data);
 
-  return (
-    <div className='container'>
-      <CatInfo catId={catId} />
-      <CatCare
-        catId={catId}
-        showModal={showModal}
-        setShowModal={setShowModal}
-        care={care}
-        setCare={setCare}
-      />
-      {showModal && (
-        <CareCalendar
-          showModal={showModal}
-          setShowModal={setShowModal}
-          care={care}
-          setCare={setCare}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className='container-wrapper'>
+			<div className='container'>
+				<CatInfo catId={catId} />
+				<CatMap />
+				<div class='button-box'>
+					<Link to='/mycat/update'>
+						<button className='cat-info-update-button'>정보 수정하기</button>
+					</Link>
+				</div>
+				<CatCare
+					catId={catId}
+					showModal={showModal}
+					setShowModal={setShowModal}
+					careHistory={careHistory}
+					setCareHistory={setCareHistory}
+				/>
+				<div class='button-box'>
+					<Link to='/mycat/update'>
+						<button className='cat-info-update-button'>돌봄 기록 추가</button>
+					</Link>
+				</div>
+				{/* {showModal && (
+					<CareCalendar
+						showModal={showModal}
+						setShowModal={setShowModal}
+						careHistory={careHistory}
+						setCareHistory={setCareHistory}
+					/>
+				)} */}
+			</div>
+		</div>
+	);
 };
 
 export default CatDetailPage;
