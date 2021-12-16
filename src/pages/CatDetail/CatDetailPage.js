2bcdbab8b3605b059b1a566131bc0124cb47f281
seeun214 <seeunjin22@naver.com>
2021-12-16 17:19:33 +0900

    [Add]ImgUpload

--- src/pages/CatDetail/CatDetailPage.js
+++ src/pages/CatDetail/CatDetailPage.js
@@ -3,19 +3,17 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import CatCare from 'components/CatDetail/CatCare/CatCare';
 import CatInfo from 'components/CatDetail/CatInfo/CatInfo';
-import CatMap from 'components/CatDetail/CatMap/CatMap';
 import './CatDetailPage.scss';
 import { Link } from 'react-router-dom';
 import axiosInstance from 'api/customAxios';
 import axios from 'axios';
-import { map } from 'components/common/Map';
-import CatMarkerMap from 'components/common/CatMarkerMap';
 import { useNavigate } from 'react-router';
-import Modal from 'components/common/Modal';
 import { Swiper } from 'swiper/react';
 import { SwiperSlide } from 'swiper/react';
 import CatMatch from 'components/CatRegister/CatMatch/CatMatch';
 import CatMoreInfoForm from 'components/CatRegister/CatMoreInfoForm/CatMoreInfoForm';
+import CatMarkerMap from 'components/Common/CatMarkerMap';
+import Modal from 'components/Common/Modal';
 
 // let catInfoData = {
 // 	id: 2,
@@ -88,167 +86,168 @@ import CatMoreInfoForm from 'components/CatRegister/CatMoreInfoForm/CatMoreInfoF
 // ];
 
 let matchedCatData = [
-	{
-		id: 1,
-		name: '보리',
-		gender: 0,
-		neutered: 1,
-		pattern: 1,
-		locations: [
-			{
-				latitude: 37.54511236317026,
-				longitude: 126.86184575808647,
-			},
-		],
-	},
-	{
-		id: 2,
-		name: '부비',
-		gender: 0,
-		neutered: 1,
-		pattern: 1,
-		locations: [
-			{
-				latitude: 37.54732777835966,
-				longitude: 126.8609590137254,
-			},
-		],
-	},
+  {
+    id: 1,
+    name: '보리',
+    gender: 0,
+    neutered: 1,
+    pattern: 1,
+    locations: [
+      {
+        latitude: 37.54511236317026,
+        longitude: 126.86184575808647,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '부비',
+    gender: 0,
+    neutered: 1,
+    pattern: 1,
+    locations: [
+      {
+        latitude: 37.54732777835966,
+        longitude: 126.8609590137254,
+      },
+    ],
+  },
 ];
 
 const CatDetailPage = () => {
-	const { catId } = useParams();
-	const [catInfo, setCatInfo] = useState('');
-	const [catLoc, setCatLoc] = useState([]);
-	const [catImg, setCatImg] = useState([]);
-	const [careHistory, setCareHistory] = useState([]);
-	const [loaded, setLoaded] = useState(false);
-	const navigate = useNavigate();
+  const { catId } = useParams();
+  const [catInfo, setCatInfo] = useState('');
+  const [catLoc, setCatLoc] = useState([]);
+  const [catImg, setCatImg] = useState([]);
+  const [careHistory, setCareHistory] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const navigate = useNavigate();
 
-	// 새로 등록할 고양이와 매칭될 기존 고양이들 리스트
-	const [matchedCatList, setMatchedCatList] = useState(matchedCatData);
-	// 동일고양이 추천 모달을 보여줄지 여부
-	const [showModal, setShowModal] = useState(false);
-	// 일치하는 고양이가 없을때 추가정보 입력창을 보여줄지 여부
-	const [moreInfo, setMoreInfo] = useState(false);
+  // 새로 등록할 고양이와 매칭될 기존 고양이들 리스트
+  const [matchedCatList, setMatchedCatList] = useState(matchedCatData);
+  // 동일고양이 추천 모달을 보여줄지 여부
+  const [showModal, setShowModal] = useState(false);
+  // 일치하는 고양이가 없을때 추가정보 입력창을 보여줄지 여부
+  const [moreInfo, setMoreInfo] = useState(false);
 
-	const deleteMyCat = () => {
-		alert('삭제한 고양이는 복구할 수 없습니다. 정말 삭제하시겠습니까?');
+  const deleteMyCat = () => {
+    alert('삭제한 고양이는 복구할 수 없습니다. 정말 삭제하시겠습니까?');
 
-		axiosInstance
-			.patch(`user/1/cat/${catId}/selection/soft-delete`)
-			.then((res) => {
-				console.log(res);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-		navigate('/mycat');
-	};
+    axiosInstance
+      .patch(`user/1/cat/${catId}/selection/soft-delete`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    navigate('/mycat');
+  };
 
-	const findOtherCat = () => {
-		setShowModal(true);
-	};
+  const findOtherCat = () => {
+    setShowModal(true);
+  };
 
-	const closeModal = () => {
-		setShowModal(false);
-	};
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     console.log('CatDetailPage');
-		axios
-			.all([
-				axiosInstance.get(`/user/1/cat/${catId}`),
-				axiosInstance.get(`/user/1/cat/${catId}/images`),
-				axiosInstance.get(`/user/1/cat/${catId}/locations`),
-				axiosInstance.get(`/user/1/cat/${catId}/48hours-catcares`),
-			])
-			.then(
-				axios.spread((catInfoRes, catImgRes, catLocRes, careHistoryRes) => {
-					setCatInfo(catInfoRes.data);
-					setCatImg(catImgRes.data);
-					setCatLoc(catLocRes.data);
-					setCareHistory(careHistoryRes.data);
-					setLoaded(true);
-				})
-			);
-	}, []);
+    axios
+      .all([
+        axiosInstance.get(`/user/1/cat/${catId}`),
+        axiosInstance.get(`/user/1/cat/${catId}/images`),
+        axiosInstance.get(`/user/1/cat/${catId}/locations`),
+        axiosInstance.get(`/user/1/cat/${catId}/48hours-catcares`),
+      ])
+      .then(
+        axios.spread((catInfoRes, catImgRes, catLocRes, careHistoryRes) => {
+          setCatInfo(catInfoRes.data);
+          setCatImg(catImgRes.data);
+          setCatLoc(catLocRes.data);
+          setCareHistory(careHistoryRes.data);
+          setLoaded(true);
+        })
+      );
+  }, []);
 
-	return loaded ? (
-		// 기본적으로 처음에 보여질 화면
-		!moreInfo ? (
-			<div className='content-container'>
-				<CatInfo
-					catId={catId}
-					catInfo={catInfo}
-					setCatInfo={setCatInfo}
-					catImg={catImg}
-					setCatImg={setCatImg}
-				/>
+  return loaded ? (
+    // 기본적으로 처음에 보여질 화면
+    !moreInfo ? (
+      <div className='content-container'>
+        <CatInfo
+          catId={catId}
+          catInfo={catInfo}
+          setCatInfo={setCatInfo}
+          catImg={catImg}
+          setCatImg={setCatImg}
+        />
 
-				<div className='title'>최근 발견된 위치</div>
-				<CatMarkerMap
-					mapId={'cat-detail-map'}
-					catLoc={catLoc}
-					width={'100%'}
-					height={'500px'}
-				/>
+        <div className='title'>최근 발견된 위치</div>
+        <CatMarkerMap
+          mapId={'cat-detail-map'}
+          catLoc={catLoc}
+          width={'100%'}
+          height={'500px'}
+        />
 
-				<div>
-					<Link to='/mycat/update'>
-						<button className='cat-info-update-button'>정보 수정하기</button>
-					</Link>
-				</div>
+        <div>
+          <Link to='/mycat/update'>
+            <button className='cat-info-update-button'>정보 수정하기</button>
+          </Link>
+        </div>
 
-				<div className='title'>최근 48시간의 돌봄 기록</div>
-				<CatCare
-					catId={catId}
-					careHistory={careHistory}
-					setCareHistory={setCareHistory}
-				/>
+        <div className='title'>최근 48시간의 돌봄 기록</div>
+        <CatCare
+          catId={catId}
+          careHistory={careHistory}
+          setCareHistory={setCareHistory}
+        />
 
-				<div className='button-box'>
-					<button className='cat-delete-button' onClick={deleteMyCat}>
-						내 도감에서 삭제
-					</button>
-					<button className='cat-other-button' onClick={findOtherCat}>
-						다른 고양이로 등록
-					</button>
-				</div>
+        <div className='button-box'>
+          <button className='cat-delete-button' onClick={deleteMyCat}>
+            내 도감에서 삭제
+          </button>
+          <button className='cat-other-button' onClick={findOtherCat}>
+            다른 고양이로 등록
+          </button>
+        </div>
 
-				{showModal && (
-					<Modal showModal={showModal} onClose={closeModal} maskClosable={true}>
-						<div style={{ width: '800px' }}>
-							<Swiper
-								slidesPerView={1}
-								navigation
-								pagination={{ clickable: true }}>
-								{matchedCatList.map((matchedCat) => (
-									<SwiperSlide>
-										<CatMatch
-											moreInfo={moreInfo}
-											setMoreInfo={setMoreInfo}
-											matchedCat={matchedCat}
-										/>
-									</SwiperSlide>
-								))}
-							</Swiper>
-						</div>
-					</Modal>
-				)}
-			</div>
-		) : (
-			// 다른 고양이 버튼 누른 후 해당 고양이를 새로운 고양이로 등록할때 추가정보 입력하는 화면
-			<CatMoreInfoForm
-				catInfo={catInfo}
-				setCatInfo={setCatInfo}
-				catImg={catImg}
-				catLoc={catLoc}
-			/>
-		)
-	) : (
-		<div>로딩중</div>
-	);
+        {showModal && (
+          <Modal showModal={showModal} onClose={closeModal} maskClosable={true}>
+            <div style={{ width: '800px' }}>
+              <Swiper
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+              >
+                {matchedCatList.map((matchedCat) => (
+                  <SwiperSlide>
+                    <CatMatch
+                      moreInfo={moreInfo}
+                      setMoreInfo={setMoreInfo}
+                      matchedCat={matchedCat}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </Modal>
+        )}
+      </div>
+    ) : (
+      // 다른 고양이 버튼 누른 후 해당 고양이를 새로운 고양이로 등록할때 추가정보 입력하는 화면
+      <CatMoreInfoForm
+        catInfo={catInfo}
+        setCatInfo={setCatInfo}
+        catImg={catImg}
+        catLoc={catLoc}
+      />
+    )
+  ) : (
+    <div>로딩중</div>
+  );
 };
 
 export default CatDetailPage;
--- src/pages/CatRegisterPage/CatRegisterPage.js
+++ src/pages/CatRegisterPage/CatRegisterPage.js
@@ -6,134 +6,135 @@ import catApi from 'api/catApi';
 import React, { useState } from 'react';
 import './CatRegisterPage.scss';
 import axiosInstance from 'api/customAxios';
-import Modal from 'components/common/Modal';
 import CatMatch from 'components/CatRegister/CatMatch/CatMatch';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import CatMoreInfoForm from 'components/CatRegister/CatMoreInfoForm/CatMoreInfoForm';
+import Modal from 'components/Common/Modal';
 
 let matchedCatData = [
-	{
-		id: 1,
-		name: '보리',
-		gender: 0,
-		neutered: 1,
-		pattern: 1,
-		locations: [
-			{
-				latitude: 37.54511236317026,
-				longitude: 126.86184575808647,
-			},
-		],
-	},
-	{
-		id: 2,
-		name: '부비',
-		gender: 0,
-		neutered: 1,
-		pattern: 1,
-		locations: [
-			{
-				latitude: 37.54732777835966,
-				longitude: 126.8609590137254,
-			},
-		],
-	},
+  {
+    id: 1,
+    name: '보리',
+    gender: 0,
+    neutered: 1,
+    pattern: 1,
+    locations: [
+      {
+        latitude: 37.54511236317026,
+        longitude: 126.86184575808647,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '부비',
+    gender: 0,
+    neutered: 1,
+    pattern: 1,
+    locations: [
+      {
+        latitude: 37.54732777835966,
+        longitude: 126.8609590137254,
+      },
+    ],
+  },
 ];
 
 const CatRegisterPage = () => {
-	// 새로 등록할 고양이 정보
-	const [catInfo, setCatInfo] = useState({
-		name: '',
-		gender: '',
-		neutered: '',
-		status: '',
-		pattern: '',
-	});
-	const [catLoc, setCatLoc] = useState([]);
-	const [catImg, setCatImg] = useState([]);
+  // 새로 등록할 고양이 정보
+  const [catInfo, setCatInfo] = useState({
+    name: '',
+    gender: '',
+    neutered: '',
+    status: '',
+    pattern: '',
+  });
+  const [catLoc, setCatLoc] = useState([]);
+  const [catImg, setCatImg] = useState([]);
 
-	// 새로 등록할 고양이와 매칭될 기존 고양이들 리스트
-	const [matchedCatList, setMatchedCatList] = useState(matchedCatData);
-	// 동일고양이 추천 모달을 보여줄지 여부
-	const [showModal, setShowModal] = useState(false);
-	// 일치하는 고양이가 없을때 추가정보 입력창을 보여줄지 여부
-	const [moreInfo, setMoreInfo] = useState(false);
+  // 새로 등록할 고양이와 매칭될 기존 고양이들 리스트
+  const [matchedCatList, setMatchedCatList] = useState(matchedCatData);
+  // 동일고양이 추천 모달을 보여줄지 여부
+  const [showModal, setShowModal] = useState(false);
+  // 일치하는 고양이가 없을때 추가정보 입력창을 보여줄지 여부
+  const [moreInfo, setMoreInfo] = useState(false);
 
-	const closeModal = () => {
-		setShowModal(false);
-	};
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
-	const handleSubmit = () => {
-		console.log(catImg); // fileList 객체타입
-		console.log(catInfo);
-		console.log(catLoc); // 기본 array 타입
+  const handleSubmit = () => {
+    console.log(catImg); // fileList 객체타입
+    console.log(catInfo);
+    console.log(catLoc); // 기본 array 타입
 
-		if (!catInfo.neutered || !catInfo.gender || !catInfo.pattern) {
-			document.getElementById('message').innerText =
-				'모든 항목을 입력해주세요!';
-		} else {
-			if (catLoc.length === 0) {
-				document.getElementById('message').innerText =
-					'1곳 이상의 위치를 선택해주세요!';
-			} else {
-				document.getElementById('message').innerText = '';
-				// 동일 추정 고양이 모달 팝업
-				setShowModal(true);
-			}
-		}
-	};
+    if (!catInfo.neutered || !catInfo.gender || !catInfo.pattern) {
+      document.getElementById('message').innerText =
+        '모든 항목을 입력해주세요!';
+    } else {
+      if (catLoc.length === 0) {
+        document.getElementById('message').innerText =
+          '1곳 이상의 위치를 선택해주세요!';
+      } else {
+        document.getElementById('message').innerText = '';
+        // 동일 추정 고양이 모달 팝업
+        setShowModal(true);
+      }
+    }
+  };
 
-	return !moreInfo ? (
-		<div className='content-container'>
-			<span className='cat-img-form'>
-				<CatImageUpload image={catImg} setImage={setCatImg} />
-			</span>
-			<span className='cat-info-form'>
-				<CatRegisterForm catInfo={catInfo} setCatInfo={setCatInfo} />
-			</span>
-			<span className='cat-map'>
-				<CatLocationMap catLoc={catLoc} setCatLoc={setCatLoc} />
-			</span>
-			<div id='message' className='warning-message'></div>
-			<div className='button-box'>
-				<button className='cancel-button'>취소하기</button>
-				<button className='submit-button' onClick={handleSubmit}>
-					등록하기
-				</button>
-			</div>
+  return !moreInfo ? (
+    <div className='content-container'>
+      <span className='cat-img-form'>
+        <CatImageUpload image={catImg} setImage={setCatImg} />
+      </span>
+      <span className='cat-info-form'>
+        <CatRegisterForm catInfo={catInfo} setCatInfo={setCatInfo} />
+      </span>
+      <span className='cat-map'>
+        <CatLocationMap catLoc={catLoc} setCatLoc={setCatLoc} />
+      </span>
+      <div id='message' className='warning-message'></div>
+      <div className='button-box'>
+        <button className='cancel-button'>취소하기</button>
+        <button className='submit-button' onClick={handleSubmit}>
+          등록하기
+        </button>
+      </div>
 
-			{showModal && (
-				<Modal showModal={showModal} onClose={closeModal} maskClosable={true}>
-					<div style={{ width: '800px' }}>
-						<Swiper
-							slidesPerView={1}
-							navigation
-							pagination={{ clickable: true }}>
-							{matchedCatList.map((matchedCat) => (
-								<SwiperSlide>
-									<CatMatch
-										moreInfo={moreInfo}
-										setMoreInfo={setMoreInfo}
-										matchedCat={matchedCat}
-									/>
-								</SwiperSlide>
-							))}
-						</Swiper>
-					</div>
-				</Modal>
-			)}
-		</div>
-	) : (
-		<CatMoreInfoForm
-			catInfo={catInfo}
-			setCatInfo={setCatInfo}
-			catImg={catImg}
-			catLoc={catLoc}
-		/>
-	);
+      {showModal && (
+        <Modal showModal={showModal} onClose={closeModal} maskClosable={true}>
+          <div style={{ width: '800px' }}>
+            <Swiper
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+            >
+              {matchedCatList.map((matchedCat) => (
+                <SwiperSlide>
+                  <CatMatch
+                    moreInfo={moreInfo}
+                    setMoreInfo={setMoreInfo}
+                    matchedCat={matchedCat}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </Modal>
+      )}
+    </div>
+  ) : (
+    <CatMoreInfoForm
+      catInfo={catInfo}
+      setCatInfo={setCatInfo}
+      catImg={catImg}
+      catLoc={catLoc}
+    />
+  );
 };
 
 export default CatRegisterPage;
