2d79c44cb5a1258889b9d7f1e3a01743c462166d
hyem <74531573+hyemin-jang@users.noreply.github.com>
2021-12-26 20:20:10 +0900

    Update customAxios.js

--- src/api/customAxios.js
+++ src/api/customAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = 'http://192.168.1.252';
+axiosInstance.defaults.baseURL = 'http://3.35.129.119';
 
 axiosInstance.interceptors.request.use(
   (request) => {
