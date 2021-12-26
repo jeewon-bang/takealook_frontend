6af8e3cbc0023d80e0c2af2887ec57d9a91a51f9
hyemin <hmjang28@gmail.com>
2021-12-26 09:50:59 +0900

    [Fix] 자동로그인

--- src/components/Common/ImgUpload.js
+++ src/components/Common/ImgUpload.js
@@ -4,22 +4,21 @@ import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const ImgUpload = (props) => {
-  const { pastImg, img, setImg } = props;
-  const [fileUrl, setFileUrl] = useState('');
+	const { pastImg, img, setImg } = props;
+	const [fileUrl, setFileUrl] = useState('');
 
-  const imgInput = useRef();
-  const handleClick = () => {
-    imgInput.current.click(); // imgInput이라는 ref가 걸린 대상(= input type='file')이 클릭되도록 한다
-  };
+	const imgInput = useRef();
+	const handleClick = () => {
+		imgInput.current.click(); // imgInput이라는 ref가 걸린 대상(= input type='file')이 클릭되도록 한다
+	};
 
-  const processImage = (e) => {
-    setImg(e.target.files);
-    const imageFile = e.target.files[0];
-    const imageUrl = URL.createObjectURL(imageFile);
-    setFileUrl(imageUrl);
-  };
+	const processImage = (e) => {
+		setImg(e.target.files);
+		const imageFile = e.target.files[0];
+		const imageUrl = URL.createObjectURL(imageFile);
+		setFileUrl(imageUrl);
+	};
 
-<<<<<<< HEAD
 	return (
 		<div>
 			<StyledMainImgBox>
@@ -73,63 +72,6 @@ const ImgUpload = (props) => {
 			</StyledMainImgBox>
 		</div>
 	);
-=======
-  return (
-    <div>
-      <div className='cat-mainimg-upload-box'>
-        <input
-          ref={imgInput}
-          className='img-input'
-          type='file'
-          multiple
-          accept='image/*'
-          name='file'
-          style={{ display: 'none' }}
-          onChange={processImage}
-        />
-        {!fileUrl ? (
-          <button
-            className='oneimg-upload-button'
-            onClick={handleClick}
-            style={{
-              backgroundColor: 'transparent',
-              padding: '0',
-            }}
-          >
-            {!pastImg ? (
-              <div>
-                <FontAwesomeIcon icon={faCamera} />
-                <br />0 / 1
-              </div>
-            ) : (
-              <div
-                style={{
-                  backgroundImage: `url(${pastImg})`,
-                  width: '200px',
-                  height: '200px',
-                  backgroundSize: '200px',
-                  backgroundPosition: 'center center',
-                  borderRadius: '5px',
-                }}
-              ></div>
-            )}
-          </button>
-        ) : (
-          <div
-            className='mainimg-preview'
-            style={{
-              backgroundImage: `url(${fileUrl})`,
-              width: '200px',
-              height: '200px',
-              backgroundSize: '200px',
-              backgroundPosition: 'center',
-            }}
-          ></div>
-        )}
-      </div>
-    </div>
-  );
->>>>>>> 21b29d604fbf6e15947e9134d2022c035aee679c
 };
 
 const StyledMainImgBox = styled.div`
